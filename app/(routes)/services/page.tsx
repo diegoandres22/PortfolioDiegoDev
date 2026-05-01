119a900612e4a01fe5856b617034625f52b9ebf0
diegoandres22 <diego.a.v3005@gmail.com>
2026-04-30 20:49:25 -0400

    FIX: add diego´s caricature

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -10,11 +10,11 @@ const ServicesPage = () => {
             <TransitionPage />
             <CircleImage />
             <AvatarServices />
-            <div className="grid mt-24 px-8 md:px-auto md:mt-auto items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+            <div className="grid mt-24 px-8 md:px-auto md:mt-auto items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 ">
                 <div className="max-w-[450px]">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Lo que mas me <span className="font-bold text-secondary"> gusta</span> 😍</h1>
-                    <p className="mb-8 text-xl text-gray-300">Desarrollador web apasionado por encontrar formas más inteligentes y sencillasde hacer las cosas. Vivo optimizando procesos y operaciones, apoyándome tanto en desarrollo a medida como en herramientas corporativas ya existentes, siempre con el objetivo de ahorrar tiempo y reducir costos.</p>
+                    <p className="z-50 mb-8 text-xl text-gray-300">Desarrollador web apasionado por encontrar formas más inteligentes y sencillas de hacer las cosas. Vivo optimizando procesos y operaciones, apoyándome tanto en desarrollo a medida como en herramientas corporativas ya existentes, siempre con el objetivo de ahorrar tiempo, minimizar el margen de error y reducir costos.</p>
                     <a href="mailto:diego.a.v3005@gmail.com"
                         className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
                         Enviar un correo
--- components/avatar-services.tsx
+++ components/avatar-services.tsx
@@ -3,8 +3,8 @@ import { MotionTransition } from "./transition-component";
 
 const AvatarServices = () => {
     return (
-        <MotionTransition position='right' className="bottom-0 left-0 hidden md:inline-block md:absolute">
-            <Image src="/services.png" width="400" height="400" className="w-[350px] h-full " alt="Particles " />
+        <MotionTransition position='right' className="bottom-0 left-0 md:inline-block absolute">
+            <Image src="/services.png" width="400" height="400" className="opacity-30 md:opacity-50 2xl:opacity-100 w-[350px] h-full " alt="Particles " />
         </MotionTransition>
     );
 }
